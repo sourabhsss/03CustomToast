55d21d378bbf293f0cafd51ee1c81c90be648b08
Sourabh Upreti <sourabhupreti16@gmail.com>
2018-12-16 14:14:29 +0530

    Initial commit

---
+++ app/src/main/java/com/techyrh/sourabhupreti/customtoast/MainActivity.java
@@ -0,0 +1,18 @@
+package com.techyrh.sourabhupreti.customtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void setred(View view) {
+        Toast.makeText(this,"Hello", Toast.LENGTH_SHORT).show();
+    }
+}
